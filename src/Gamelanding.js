eedd0ad0b9e9dffd0ef5752e164e911fe4f9dee9
Haavisto Heli <s2100133@edu.bc.fi>
2021-05-24 16:32:39 +0300

    added gamelanding.js without styling and alphabet& dragndrop Links to main.js

---
+++ src/Gamelanding.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+const Gamelanding = () => {
+    return (
+        <div>
+            <h2>Let's play</h2>
+            <h1>Drag'n'Drop</h1>
+            <h3>CHOOSE AND MATCH CORRECT TRANSLATIONS BY DRAGGING AND DROPPING</h3>
+            <Link to="/dragndrop">PLAY</Link><br></br>
+            <Link to="/">HOME</Link>
+
+        </div>
+    );
+};
+
+export default Gamelanding;
