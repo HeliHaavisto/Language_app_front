1f27f38220f883b862dcc1ed91aa44d6b722d060
sagar-aryal <sagar_yal92@outlook.com>
2021-05-24 15:48:36 +0300

    Routing, Home and Menu done without styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import "./App.css";
-import Home from "./components/Home";
+
+import Home from "./Home";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import About from "./About";
+import Alphabet from "./Alphabet";
+import Dragndrop from "./Dragndrop";
+import Gamelanding from "./Gamelanding";
+import Menu from "./Menu";
+import Modal from "./Modal";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/alphabet">
+          <Alphabet />
+        </Route>
+        <Route path="/dragndrop">
+          <Dragndrop />
+        </Route>
+        <Route path="/gamelanding">
+          <Gamelanding />
+        </Route>
+        <Route path="/menu">
+          <Menu />
+        </Route>
+        <Route path="/modal">
+          <Modal />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/Menu.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Menu = () => {
+  return (
+    <div>
+      <h1>Alphabets or Drag'n'Drop</h1>
+
+      <Link to="/">Home</Link>
+    </div>
+  );
+};
+
+export default Menu;
