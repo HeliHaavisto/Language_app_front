4f4985022d1f7486d4d074133986db16dcda6936
Iolanta26 <iolanta.u@gmail.com>
2021-05-24 17:53:41 +0300

    basic css added, classes added

---
+++ src/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "./language_app_logo.png";
+
+const About = () => {
+  return (
+    <div>
+      <h1 className="style-font">About</h1>
+      <img src={Logo} alt="logo" style={{ width: "5rem" }} />
+      <p>
+        This is our team project made by 4 awesome students : Aryal Sagar,
+        Ivankina Elena, Haavisto Heli and Iolanta Ubozhenko.
+      </p>
+      <p>Frond end is made with React Js.</p>
+      <Link to="/">Home</Link>
+    </div>
+  );
+};
+
+export default About;
