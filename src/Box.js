d4abbf9a79ed121568173f9962d3c390ffb83d10
sagar-aryal <sagar_yal92@outlook.com>
2021-05-27 10:32:48 +0300

    draganddrop progress

--- src/Box.js
+++ src/Box.js
@@ -10,14 +10,51 @@ const Box = () => {
     });
   }, []);
 
+  const onDragStartHandler = (e, dragId) => {
+    //console.log("dragstart", dragId);
+    e.dataTransfer.setData("id", dragId);
+  };
+
+  const onDragOverHandler = (e) => {
+    e.preventDefault();
+  };
+
+  const onDropHandler = (e, dropId) => {
+    let dragId = e.dataTransfer.getData("id");
+    console.log(dragId);
+    console.log(dropId);
+    if (dragId === dropId) {
+      e.target.splice();
+    } else {
+      return;
+    }
+  };
+
   return (
     <div>
       {data.map((item) => {
         return (
           <div key={item.id}>
-            <strong className="drag ">{item.name_fi}</strong>
-
-            <img className="drop drag " src={item.img} alt={item.name_fi} />
+            <img
+              onDragStart={(e) => {
+                return onDragStartHandler(e, item.id);
+              }}
+              draggable
+              className="drag "
+              src={item.img}
+              alt={item.name_fi}
+            />
+            <strong
+              onDragOver={(e) => {
+                return onDragOverHandler(e);
+              }}
+              onDrop={(e) => {
+                return onDropHandler(e, item.id);
+              }}
+              className=" drop"
+            >
+              {item.name_fi}
+            </strong>
           </div>
         );
       })}
