22562fcc0437a4a3e0148b8b2c22253ae80d0160
sagar-aryal <sagar_yal92@outlook.com>
2021-05-28 09:46:50 +0300

    Pagination added and dragndrop still need to fix

--- src/Box.js
+++ src/Box.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Pagination from "./Pagination";
 
 const Box = () => {
   const [data, setData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(4);
 
   useEffect(() => {
     axios.get("http://localhost:3001/items").then((response) => {
@@ -11,7 +14,7 @@ const Box = () => {
   }, []);
 
   const onDragStartHandler = (e, dragId) => {
-    //console.log("dragstart", dragId);
+    //console.log(dragId);
     e.dataTransfer.setData("id", dragId);
   };
 
@@ -20,37 +23,43 @@ const Box = () => {
   };
 
   const onDropHandler = (e, dropId) => {
+    e.preventDefault();
     let dragId = e.dataTransfer.getData("id");
-    console.log(dragId);
-    console.log(dropId);
-    if (dragId === dropId) {
-      e.target.splice();
+    //console.log(dragId);
+    //console.log(dropId);
+
+    if (dragId == dropId) {
+      e.target.style.backgroundColor = "green";
     } else {
-      return;
+      e.target.style.backgroundColor = "red";
     }
+  };
+
+  //Get current posts
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = data.slice(indexOfFirstPost, indexOfLastPost);
+
+  //Change page
+  const paginate = (pageNumber) => {
+    return setCurrentPage(pageNumber);
   };
 
   return (
     <div>
-      {data.map((item) => {
+      {currentPosts.map((item) => {
         return (
           <div key={item.id}>
             <img
-              onDragStart={(e) => {
-                return onDragStartHandler(e, item.id);
-              }}
-              draggable
               className="drag "
               src={item.img}
               alt={item.name_fi}
+              draggable
+              onDragStart={(e) => onDragStartHandler(e, item.id)}
             />
             <strong
-              onDragOver={(e) => {
-                return onDragOverHandler(e);
-              }}
-              onDrop={(e) => {
-                return onDropHandler(e, item.id);
-              }}
+              onDragOver={(e) => onDragOverHandler(e)}
+              onDrop={(e) => onDropHandler(e, item.id)}
               className=" drop"
             >
               {item.name_fi}
@@ -58,6 +67,11 @@ const Box = () => {
           </div>
         );
       })}
+      <Pagination
+        postsPerPage={postsPerPage}
+        totalPosts={data.length}
+        paginate={paginate}
+      />
     </div>
   );
 };
