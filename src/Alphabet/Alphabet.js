508865986b104c9b0551f0b56b41978c380babcd
Elena <ivankina.es@gmail.com>
2021-06-01 17:17:49 +0300

    latest version

--- src/Alphabet/Alphabet.js
+++ src/Alphabet/Alphabet.js
@@ -37,157 +37,157 @@ import soundDotsA from "./sounds/ä.mp3";
 import soundDotsO from "./sounds/ö.mp3";
 
 class Alphabet extends Component {
-  state = {
-    abc: abc,
-  };
+	state = {
+		abc: abc,
+	};
 
-  clickHandler = (abc_name) => {
-    switch (abc_name) {
-      case "Aa":
-        let a_sound = new Audio(soundA);
-        a_sound.play();
-        break;
-      case "Bb":
-        let b_sound = new Audio(soundB);
-        b_sound.play();
-        break;
-      case "Cc":
-        let c_sound = new Audio(soundC);
-        c_sound.play();
-        break;
-      case "Dd":
-        let d_sound = new Audio(soundD);
-        d_sound.play();
-        break;
-      case "Ee":
-        let e_sound = new Audio(soundE);
-        e_sound.play();
-        break;
-      case "Ff":
-        let f_sound = new Audio(soundF);
-        f_sound.play();
-        break;
-      case "Gg":
-        let g_sound = new Audio(soundG);
-        g_sound.play();
-        break;
-      case "Hh":
-        let h_sound = new Audio(soundH);
-        h_sound.play();
-        break;
-      case "Ii":
-        let i_sound = new Audio(soundI);
-        i_sound.play();
-        break;
-      case "Jj":
-        let j_sound = new Audio(soundJ);
-        j_sound.play();
-        break;
-      case "Kk":
-        let k_sound = new Audio(soundK);
-        k_sound.play();
-        break;
-      case "Ll":
-        let l_sound = new Audio(soundL);
-        l_sound.play();
-        break;
-      case "Mm":
-        let m_sound = new Audio(soundM);
-        m_sound.play();
-        break;
-      case "Nn":
-        let n_sound = new Audio(soundN);
-        n_sound.play();
-        break;
-      case "Oo":
-        let o_sound = new Audio(soundO);
-        o_sound.play();
-        break;
-      case "Pp":
-        let p_sound = new Audio(soundP);
-        p_sound.play();
-        break;
-      case "Qq":
-        let q_sound = new Audio(soundQ);
-        q_sound.play();
-        break;
-      case "Rr":
-        let r_sound = new Audio(soundR);
-        r_sound.play();
-        break;
-      case "Ss":
-        let s_sound = new Audio(soundS);
-        s_sound.play();
-        break;
-      case "Tt":
-        let t_sound = new Audio(soundT);
-        t_sound.play();
-        break;
-      case "Uu":
-        let u_sound = new Audio(soundU);
-        u_sound.play();
-        break;
-      case "Vv":
-        let v_sound = new Audio(soundV);
-        v_sound.play();
-        break;
-      case "Ww":
-        let w_sound = new Audio(soundW);
-        w_sound.play();
-        break;
-      case "Xx":
-        let x_sound = new Audio(soundX);
-        x_sound.play();
-        break;
-      case "Yy":
-        let y_sound = new Audio(soundY);
-        y_sound.play();
-        break;
-      case "Zz":
-        let z_sound = new Audio(soundZ);
-        z_sound.play();
-        break;
-      case "Åå":
-        let swed_a_sound = new Audio(soundSwedA);
-        swed_a_sound.play();
-        break;
-      case "Ää":
-        let dots_a_sound = new Audio(soundDotsA);
-        dots_a_sound.play();
-        break;
-      case "Öö":
-        let dots_o_sound = new Audio(soundDotsO);
-        dots_o_sound.play();
-        break;
+	clickHandler = (abc_name) => {
+		switch (abc_name) {
+			case "Aa":
+				let a_sound = new Audio(soundA);
+				a_sound.play();
+				break;
+			case "Bb":
+				let b_sound = new Audio(soundB);
+				b_sound.play();
+				break;
+			case "Cc":
+				let c_sound = new Audio(soundC);
+				c_sound.play();
+				break;
+			case "Dd":
+				let d_sound = new Audio(soundD);
+				d_sound.play();
+				break;
+			case "Ee":
+				let e_sound = new Audio(soundE);
+				e_sound.play();
+				break;
+			case "Ff":
+				let f_sound = new Audio(soundF);
+				f_sound.play();
+				break;
+			case "Gg":
+				let g_sound = new Audio(soundG);
+				g_sound.play();
+				break;
+			case "Hh":
+				let h_sound = new Audio(soundH);
+				h_sound.play();
+				break;
+			case "Ii":
+				let i_sound = new Audio(soundI);
+				i_sound.play();
+				break;
+			case "Jj":
+				let j_sound = new Audio(soundJ);
+				j_sound.play();
+				break;
+			case "Kk":
+				let k_sound = new Audio(soundK);
+				k_sound.play();
+				break;
+			case "Ll":
+				let l_sound = new Audio(soundL);
+				l_sound.play();
+				break;
+			case "Mm":
+				let m_sound = new Audio(soundM);
+				m_sound.play();
+				break;
+			case "Nn":
+				let n_sound = new Audio(soundN);
+				n_sound.play();
+				break;
+			case "Oo":
+				let o_sound = new Audio(soundO);
+				o_sound.play();
+				break;
+			case "Pp":
+				let p_sound = new Audio(soundP);
+				p_sound.play();
+				break;
+			case "Qq":
+				let q_sound = new Audio(soundQ);
+				q_sound.play();
+				break;
+			case "Rr":
+				let r_sound = new Audio(soundR);
+				r_sound.play();
+				break;
+			case "Ss":
+				let s_sound = new Audio(soundS);
+				s_sound.play();
+				break;
+			case "Tt":
+				let t_sound = new Audio(soundT);
+				t_sound.play();
+				break;
+			case "Uu":
+				let u_sound = new Audio(soundU);
+				u_sound.play();
+				break;
+			case "Vv":
+				let v_sound = new Audio(soundV);
+				v_sound.play();
+				break;
+			case "Ww":
+				let w_sound = new Audio(soundW);
+				w_sound.play();
+				break;
+			case "Xx":
+				let x_sound = new Audio(soundX);
+				x_sound.play();
+				break;
+			case "Yy":
+				let y_sound = new Audio(soundY);
+				y_sound.play();
+				break;
+			case "Zz":
+				let z_sound = new Audio(soundZ);
+				z_sound.play();
+				break;
+			case "Åå":
+				let swed_a_sound = new Audio(soundSwedA);
+				swed_a_sound.play();
+				break;
+			case "Ää":
+				let dots_a_sound = new Audio(soundDotsA);
+				dots_a_sound.play();
+				break;
+			case "Öö":
+				let dots_o_sound = new Audio(soundDotsO);
+				dots_o_sound.play();
+				break;
 
-      default:
-        console.log("Error from sound");
-        break;
-    }
-  };
+			default:
+				console.log("Error from sound");
+				break;
+		}
+	};
 
-  render() {
-    return (
-      <>
-        <div className="p-alphabet">
-          <p>Press each letter to learn pronunciation</p>
-        </div>
-        <div className="abc-container">
-          {this.state.abc.map((letter) => {
-            return (
-              <Letter
-                key={letter.name}
-                name={letter.name}
-                pronunciation={letter.pronunciation}
-                play_sound={this.clickHandler.bind(this, letter.name)}
-              />
-            );
-          })}
-        </div>
-        <Link to="/">Home</Link>
-      </>
-    );
-  }
+	render() {
+		return (
+			<div className="container">
+				<div className="p-alphabet">
+					<p>Press each letter to learn pronunciation</p>
+				</div>
+				<div className="abc-container">
+					{this.state.abc.map((letter) => {
+						return (
+							<Letter
+								key={letter.name}
+								name={letter.name}
+								pronunciation={letter.pronunciation}
+								play_sound={this.clickHandler.bind(this, letter.name)}
+							/>
+						);
+					})}
+				</div>
+				<Link to="/">Home</Link>
+			</div>
+		);
+	}
 }
 
 export default Alphabet;
