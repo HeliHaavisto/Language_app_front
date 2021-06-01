204e1ad03ac348e8edd448b29bcf30d370c5cffd
Elena <ivankina.es@gmail.com>
2021-06-01 18:23:15 +0300

    moved buttons to the modal window

--- src/DragNDrop/DragNDrop.js
+++ src/DragNDrop/DragNDrop.js
@@ -12,317 +12,321 @@ import "./Drag.css";
 import randomArrayShuffle from "./shuffle";
 
 const DragNDrop = () => {
-  const [data, setData] = useState([]);
-  const dataSet1 = data.slice(0, 5);
-  const dataSet2 = data.slice(5, 10);
-  const dataSet3 = data.slice(10, 15);
-  const dataSet4 = data.slice(15, 20);
+	const [data, setData] = useState([]);
+	const dataSet1 = data.slice(0, 5);
+	const dataSet2 = data.slice(5, 10);
+	const dataSet3 = data.slice(10, 15);
+	const dataSet4 = data.slice(15, 20);
 
-  const [counter, setCounter] = useState(1);
-  const [loading, setLoading] = useState(false);
+	const [counter, setCounter] = useState(1);
+	const [loading, setLoading] = useState(false);
+	// const [correctCounter, setCorrectCounter] = useState(0);
 
-  const [modalShow, setModalShow] = React.useState(false);
+	const [modalShow, setModalShow] = React.useState(false);
 
-  useEffect(() => {
-    axios
-      .get("http://warm-beach-10313.herokuapp.com/items/all")
-      .then((response) => {
-        setData(response.data);
-        setLoading(true);
-      });
-  }, []);
+	useEffect(() => {
+		axios.get("http://warm-beach-10313.herokuapp.com/items/all").then((response) => {
+			setData(response.data);
+			setLoading(true);
+		});
+	}, []);
 
-  function dragStart(event) {
-    event.dataTransfer.setData("text", event.target.id); // or "text/plain" but just "text" would also be fine since we are not setting any other type/format for data value
-    console.log("START DRAGGING!");
-  }
+	function dragStart(event) {
+		event.dataTransfer.setData("text", event.target.id); // or "text/plain" but just "text" would also be fine since we are not setting any other type/format for data value
+	}
 
-  function dragEnter(event) {
-    if (!event.target.classList.contains("dropped")) {
-      event.target.classList.add("droppable-hover");
-    }
-    console.log("ENTERED DRAGGING");
-  }
+	function dragEnter(event) {
+		if (!event.target.classList.contains("dropped")) {
+			event.target.classList.add("droppable-hover");
+		}
+	}
 
-  function dragOver(event) {
-    if (!event.target.classList.contains("dropped")) {
-      event.preventDefault(); // Prevent default to allow drop
-    }
-    console.log("DRAG OVER!");
-  }
+	function dragOver(event) {
+		if (!event.target.classList.contains("dropped")) {
+			event.preventDefault(); // Prevent default to allow drop
+		}
+	}
 
-  function dragLeave(event) {
-    if (!event.target.classList.contains("dropped")) {
-      event.target.classList.remove("droppable-hover");
-    }
-    console.log("LEAVE DRAGGING!");
-  }
+	function dragLeave(event) {
+		if (!event.target.classList.contains("dropped")) {
+			event.target.classList.remove("droppable-hover");
+		}
+	}
+	let correctCounter = 0;
+	function drop(event) {
+		event.preventDefault(); // This is in order to prevent the browser default handling of the data
+		event.target.classList.remove("droppable-hover");
+		const draggableElementData = event.dataTransfer.getData("text"); // Get the dragged data. This method will return any data that was set to the same type in the setData() method
+		const droppableElementData = event.target.getAttribute("data-draggable-id");
+		const droppableFinnishName = event.target.getAttribute("finnish_name");
+		const isCorrectMatching = draggableElementData === droppableElementData;
+		if (isCorrectMatching) {
+			const draggableElement = document.getElementById(draggableElementData);
+			event.target.classList.add("dropped");
+			draggableElement.classList.add("dragged");
+			draggableElement.setAttribute("draggable", "false");
+			event.target.innerHTML = `<img src="./img/${draggableElementData}.jpg"/><span>${droppableFinnishName}</span>`;
+			correctCounter = correctCounter + 1;
+			if (correctCounter === 5) {
+				setModalShow(true);
+			}
 
-  function drop(event) {
-    event.preventDefault(); // This is in order to prevent the browser default handling of the data
-    event.target.classList.remove("droppable-hover");
-    const draggableElementData = event.dataTransfer.getData("text"); // Get the dragged data. This method will return any data that was set to the same type in the setData() method
-    const droppableElementData = event.target.getAttribute("data-draggable-id");
-    const droppableFinnishName = event.target.getAttribute("finnish_name");
-    const isCorrectMatching = draggableElementData === droppableElementData;
-    if (isCorrectMatching) {
-      const draggableElement = document.getElementById(draggableElementData);
-      event.target.classList.add("dropped");
-      draggableElement.classList.add("dragged");
-      draggableElement.setAttribute("draggable", "false");
-      event.target.innerHTML = `<img src="./img/${draggableElementData}.jpg"/><span>${droppableFinnishName}</span>`;
-    }
-  }
+			// setCorrectCounter(correctCounter + 1);
+			console.log(correctCounter);
+		}
+	}
 
-  function startOver() {
-    setCounter(1);
-  }
+	function startOver() {
+		setCounter(1);
+		setModalShow(false);
+	}
 
-  function nextLevel() {
-    setCounter(counter + 1);
-  }
+	function nextLevel() {
+		setCounter(counter + 1);
+		setModalShow(false);
+	}
 
-  return (
-    <div className="container">
-      <h1 className="style-font">Level {counter}</h1>
-      <div className="container-for-elements">
-        {loading === false && (
-          <Spinner className="spinner" animation="border" variant="warning" />
-        )}
-        {counter === 1 && (
-          <>
-            <section className="draggable-elements">
-              {randomArrayShuffle(
-                dataSet1.map((item) => {
-                  return (
-                    <img
-                      alt={item.name_en}
-                      src={item.img}
-                      className="draggable"
-                      key={item.name_en}
-                      draggable
-                      id={item.name_en}
-                      onDragStart={(e) => {
-                        dragStart(e);
-                      }}
-                    />
-                  );
-                })
-              )}
-            </section>
-            <section className="droppable-elements">
-              {randomArrayShuffle(
-                dataSet1.map((item) => {
-                  return (
-                    <div
-                      finnish_name={item.name_fi}
-                      key={item.name_en}
-                      className="droppable"
-                      data-draggable-id={item.name_en}
-                      onDragEnter={(e) => {
-                        dragEnter(e);
-                      }}
-                      onDragOver={(e) => {
-                        dragOver(e);
-                      }}
-                      onDragLeave={(e) => {
-                        dragLeave(e);
-                      }}
-                      onDrop={(e) => {
-                        drop(e);
-                      }}
-                    >
-                      <span>{item.name_fi}</span>
-                    </div>
-                  );
-                })
-              )}
-            </section>
-          </>
-        )}
-        {counter === 2 && (
-          <>
-            <section className="draggable-elements">
-              {randomArrayShuffle(
-                dataSet2.map((item) => {
-                  return (
-                    <img
-                      alt={item.name_en}
-                      src={item.img}
-                      className="draggable"
-                      key={item.name_en}
-                      draggable
-                      id={item.name_en}
-                      onDragStart={(e) => {
-                        dragStart(e);
-                      }}
-                    />
-                  );
-                })
-              )}
-            </section>
-            <section className="droppable-elements">
-              {randomArrayShuffle(
-                dataSet2.map((item) => {
-                  return (
-                    <div
-                      finnish_name={item.name_fi}
-                      key={item.name_en}
-                      className="droppable"
-                      data-draggable-id={item.name_en}
-                      onDragEnter={(e) => {
-                        dragEnter(e);
-                      }}
-                      onDragOver={(e) => {
-                        dragOver(e);
-                      }}
-                      onDragLeave={(e) => {
-                        dragLeave(e);
-                      }}
-                      onDrop={(e) => {
-                        drop(e);
-                      }}
-                    >
-                      <span>{item.name_fi}</span>
-                    </div>
-                  );
-                })
-              )}
-            </section>
-          </>
-        )}
-        {counter === 3 && (
-          <>
-            <section className="draggable-elements">
-              {randomArrayShuffle(
-                dataSet3.map((item) => {
-                  return (
-                    <img
-                      alt={item.name_en}
-                      src={item.img}
-                      className="draggable"
-                      key={item.name_en}
-                      draggable
-                      id={item.name_en}
-                      onDragStart={(e) => {
-                        dragStart(e);
-                      }}
-                    />
-                  );
-                })
-              )}
-            </section>
-            <section className="droppable-elements">
-              {randomArrayShuffle(
-                dataSet3.map((item) => {
-                  return (
-                    <div
-                      finnish_name={item.name_fi}
-                      key={item.name_en}
-                      className="droppable"
-                      data-draggable-id={item.name_en}
-                      onDragEnter={(e) => {
-                        dragEnter(e);
-                      }}
-                      onDragOver={(e) => {
-                        dragOver(e);
-                      }}
-                      onDragLeave={(e) => {
-                        dragLeave(e);
-                      }}
-                      onDrop={(e) => {
-                        drop(e);
-                      }}
-                    >
-                      <span>{item.name_fi}</span>
-                    </div>
-                  );
-                })
-              )}
-            </section>
-          </>
-        )}
-        {counter === 4 && (
-          <>
-            <section className="draggable-elements">
-              {randomArrayShuffle(
-                dataSet4.map((item) => {
-                  return (
-                    <img
-                      alt={item.name_en}
-                      src={item.img}
-                      className="draggable"
-                      key={item.name_en}
-                      draggable
-                      id={item.name_en}
-                      onDragStart={(e) => {
-                        dragStart(e);
-                      }}
-                    />
-                  );
-                })
-              )}
-            </section>
-            <section className="droppable-elements">
-              {randomArrayShuffle(
-                dataSet4.map((item) => {
-                  return (
-                    <div
-                      finnish_name={item.name_fi}
-                      key={item.name_en}
-                      className="droppable"
-                      data-draggable-id={item.name_en}
-                      onDragEnter={(e) => {
-                        dragEnter(e);
-                      }}
-                      onDragOver={(e) => {
-                        dragOver(e);
-                      }}
-                      onDragLeave={(e) => {
-                        dragLeave(e);
-                      }}
-                      onDrop={(e) => {
-                        drop(e);
-                      }}
-                    >
-                      <span>{item.name_fi}</span>
-                    </div>
-                  );
-                })
-              )}
-            </section>
-          </>
-        )}
-      </div>
-      <div className="buttonsOnBottom">
-        <ModalWindow show={modalShow} onHide={() => setModalShow(false)} />
-        <Link to="/">
-          <Button variant="warning" className="dragNdropButtons">
-            Home
-          </Button>
-        </Link>
-        <Button
-          variant="warning"
-          className="dragNdropButtons"
-          onClick={startOver}
-        >
-          Start Over
-        </Button>
-        {counter < 4 && (
-          <Button
-            variant="warning"
-            className="dragNdropButtons"
-            onClick={function (event) {
-              nextLevel();
-              setModalShow(true);
-            }}
-          >
-            Next Level
-          </Button>
-        )}
-      </div>
-    </div>
-  );
+	return (
+		<div className="container">
+			{loading === false && <Spinner className="spinner" animation="border" variant="warning" />}
+			<h1 className="style-font">Level {counter}</h1>
+			<div className="container-for-elements">
+				{counter === 1 && (
+					<>
+						<section className="draggable-elements">
+							{randomArrayShuffle(
+								dataSet1.map((item) => {
+									return (
+										<img
+											alt={item.name_en}
+											src={item.img}
+											className="draggable"
+											key={item.name_en}
+											draggable
+											id={item.name_en}
+											onDragStart={(e) => {
+												dragStart(e);
+											}}
+										/>
+									);
+								})
+							)}
+						</section>
+						<section className="droppable-elements">
+							{randomArrayShuffle(
+								dataSet1.map((item) => {
+									return (
+										<div
+											finnish_name={item.name_fi}
+											key={item.name_en}
+											className="droppable"
+											data-draggable-id={item.name_en}
+											onDragEnter={(e) => {
+												dragEnter(e);
+											}}
+											onDragOver={(e) => {
+												dragOver(e);
+											}}
+											onDragLeave={(e) => {
+												dragLeave(e);
+											}}
+											onDrop={(e) => {
+												drop(e);
+											}}
+										>
+											<span>{item.name_fi}</span>
+										</div>
+									);
+								})
+							)}
+						</section>
+					</>
+				)}
+				{counter === 2 && (
+					<>
+						<section className="draggable-elements">
+							{randomArrayShuffle(
+								dataSet2.map((item) => {
+									return (
+										<img
+											alt={item.name_en}
+											src={item.img}
+											className="draggable"
+											key={item.name_en}
+											draggable
+											id={item.name_en}
+											onDragStart={(e) => {
+												dragStart(e);
+											}}
+										/>
+									);
+								})
+							)}
+						</section>
+						<section className="droppable-elements">
+							{randomArrayShuffle(
+								dataSet2.map((item) => {
+									return (
+										<div
+											finnish_name={item.name_fi}
+											key={item.name_en}
+											className="droppable"
+											data-draggable-id={item.name_en}
+											onDragEnter={(e) => {
+												dragEnter(e);
+											}}
+											onDragOver={(e) => {
+												dragOver(e);
+											}}
+											onDragLeave={(e) => {
+												dragLeave(e);
+											}}
+											onDrop={(e) => {
+												drop(e);
+											}}
+										>
+											<span>{item.name_fi}</span>
+										</div>
+									);
+								})
+							)}
+						</section>
+					</>
+				)}
+				{counter === 3 && (
+					<>
+						<section className="draggable-elements">
+							{randomArrayShuffle(
+								dataSet3.map((item) => {
+									return (
+										<img
+											alt={item.name_en}
+											src={item.img}
+											className="draggable"
+											key={item.name_en}
+											draggable
+											id={item.name_en}
+											onDragStart={(e) => {
+												dragStart(e);
+											}}
+										/>
+									);
+								})
+							)}
+						</section>
+						<section className="droppable-elements">
+							{randomArrayShuffle(
+								dataSet3.map((item) => {
+									return (
+										<div
+											finnish_name={item.name_fi}
+											key={item.name_en}
+											className="droppable"
+											data-draggable-id={item.name_en}
+											onDragEnter={(e) => {
+												dragEnter(e);
+											}}
+											onDragOver={(e) => {
+												dragOver(e);
+											}}
+											onDragLeave={(e) => {
+												dragLeave(e);
+											}}
+											onDrop={(e) => {
+												drop(e);
+											}}
+										>
+											<span>{item.name_fi}</span>
+										</div>
+									);
+								})
+							)}
+						</section>
+					</>
+				)}
+				{counter === 4 && (
+					<>
+						<section className="draggable-elements">
+							{randomArrayShuffle(
+								dataSet4.map((item) => {
+									return (
+										<img
+											alt={item.name_en}
+											src={item.img}
+											className="draggable"
+											key={item.name_en}
+											draggable
+											id={item.name_en}
+											onDragStart={(e) => {
+												dragStart(e);
+											}}
+										/>
+									);
+								})
+							)}
+						</section>
+						<section className="droppable-elements">
+							{randomArrayShuffle(
+								dataSet4.map((item) => {
+									return (
+										<div
+											finnish_name={item.name_fi}
+											key={item.name_en}
+											className="droppable"
+											data-draggable-id={item.name_en}
+											onDragEnter={(e) => {
+												dragEnter(e);
+											}}
+											onDragOver={(e) => {
+												dragOver(e);
+											}}
+											onDragLeave={(e) => {
+												dragLeave(e);
+											}}
+											onDrop={(e) => {
+												drop(e);
+											}}
+										>
+											<span>{item.name_fi}</span>
+										</div>
+									);
+								})
+							)}
+						</section>
+					</>
+				)}
+			</div>
+			<div className="buttonsOnBottom">
+				<ModalWindow
+					counter={counter}
+					startover={() => {
+						startOver();
+					}}
+					show={modalShow}
+					nextlevel={() => nextLevel()}
+					onHide={() => setModalShow(false)}
+				/>
+				<Link to="/">
+					<Button variant="warning" className="dragNdropButtons">
+						Home
+					</Button>
+				</Link>
+
+				{/* {counter < 4 && (
+					<Button
+						variant="warning"
+						className="dragNdropButtons"
+						onClick={function () {
+							nextLevel();
+							setModalShow(true);
+						}}
+					>
+						Next Level
+					</Button>
+				)} */}
+			</div>
+		</div>
+	);
 };
 
 export default DragNDrop;
--- src/DragNDrop/ModalWindow.js
+++ src/DragNDrop/ModalWindow.js
@@ -3,30 +3,33 @@ import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 
 function ModalWindow(props) {
-  return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">Woohoo</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <h4>Great! You have learned 5 new words!</h4>
-        <p>
-          Keep going to impove your skills in Finnish language. Good luck in the
-          next level.
-        </p>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="warning" onClick={props.onHide}>
-          Close
-        </Button>
-      </Modal.Footer>
-    </Modal>
-  );
+	return (
+		<Modal {...props} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
+			<Modal.Header closeButton>
+				<Modal.Title id="contained-modal-title-vcenter">Woohoo</Modal.Title>
+			</Modal.Header>
+			<Modal.Body>
+				<h4>Great! You have learned 5 new words!</h4>
+				<p>Keep going to impove your skills in Finnish language. Good luck in the next level.</p>
+			</Modal.Body>
+			<Modal.Footer>
+				{props.counter > 1 && (
+					<Button variant="danger" className="dragNdropButtons" onClick={props.startover}>
+						Start Again
+					</Button>
+				)}
+				{props.counter < 4 && (
+					<Button variant="warning" className="dragNdropButtons" onClick={props.nextlevel}>
+						Next Level
+					</Button>
+				)}
+
+				{/* <Button variant="warning" onClick={props.onHide}>
+					Close
+				</Button> */}
+			</Modal.Footer>
+		</Modal>
+	);
 }
 
 export default ModalWindow;
