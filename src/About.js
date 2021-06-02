1ac5026a2dc50dd34711310896873d0017693d81
Elena <ivankina.es@gmail.com>
2021-06-02 16:24:54 +0300

    fixed some styling on about page

--- src/About.js
+++ src/About.js
@@ -10,9 +10,9 @@ const About = () => {
 				<p>This is our team project made by 4 awesome students : </p>
 				<ul>
 					<li>Aryal Sagar</li>
-					<li>Ivankina Elena</li>
 					<li>Haavisto Heli</li>
-					<li>Iolanta Ubozhenko</li>
+					<li>Ivankina Elena</li>
+					<li>Ubozhenko Iolanta</li>
 				</ul>
 
 				<p>Frond end is made with React Js.</p>
